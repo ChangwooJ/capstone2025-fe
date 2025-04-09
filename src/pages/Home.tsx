739d86467ef530ca1e7bbc535b66ae7054435ac0
ChangwooJ <wjdckddn99@naver.com>
2025-04-09 15:09:35 +0900

    Feat: 임시 소개페이지

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,9 +9,23 @@ const HomeWrapper = styled.div`
 const IntroduceSection = styled.div`
   border: 1px solid red;
   display: flex;
+  flex-direction: column;
   justify-content: center;
+  align-items: center;
   width: 100%;
   height: 80%;
+  background-color: #0b45c2;
+`;
+
+const IntroduceDescription = styled.div`
+  font-size: 1.6rem;
+  margin-top: 10%;
+  font-weight: bold;
+  color: white;
+`;
+
+const IntroduceSubDescription = styled.div`
+  color: white; 
 `;
 
 const IntroduceWrapper = styled.div`
@@ -53,6 +67,12 @@ const Home = () => {
   return (
     <HomeWrapper>
       <IntroduceSection>
+        <IntroduceDescription>
+          편리하고 정확한 AI 디지털 자산 거래소
+        </IntroduceDescription>
+        <IntroduceSubDescription>
+          AI를 이용해 편리한 자동매매 거래소를 이용해보세요
+        </IntroduceSubDescription>
         <IntroduceWrapper>
           <IntroduceButton onClick={() => navigate("exchange")}>
             거래소 둘러보기
